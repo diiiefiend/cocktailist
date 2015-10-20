7e58a0007b60f1c3834acaf9e5ec64e4adf62cff
Ariel <arielqhou@gmail.com>
2015-10-20 02:30:19 -0400

    Add google Places data: hours and rating.

--- app/assets/javascripts/views/cocktails/catShow.js
+++ app/assets/javascripts/views/cocktails/catShow.js
@@ -86,6 +86,8 @@ Cocktailist.Views.CocktailCat= Backbone.LiquorView.extend({
       center: coords,
       zoom: 15,
       maxZoom: 15,
+      scrollwheel: false,
+      disableDoubleClickZoom: false,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
       draggable: false,
       overviewMapControl: false
@@ -115,10 +117,47 @@ Cocktailist.Views.CocktailCat= Backbone.LiquorView.extend({
     map.fitBounds(markerBounds);
 
     if(this.filterType === 'liquor'){
+
       var liquors = this.liquorTypes();
       var bars = this.bars(this.catCollection);
       var temp = this.template['liquorTemp']({category: this.category, liquors: liquors, bars: bars, target: target});
+
     } else if(this.filterType === 'bar'){
+
+      //retrieve some info on the bar, per Google Places API
+
+      var request = {
+        location: map.getCenter(),
+        radius: '500',
+        query: this.category
+      };
+      var service = new google.maps.places.PlacesService(map);
+      service.textSearch(request, function (results, status){
+        if(status == google.maps.places.PlacesServiceStatus.OK){
+          service.getDetails({placeId: results[0].place_id}, function (place, status2){
+            if(status2 == google.maps.places.PlacesServiceStatus.OK){
+              var $barInfo = this.$el.find(".bar-info");
+              $barInfo.html("<p>Google Rating: " + place.rating + "</p><ul>");
+              var hours = place.opening_hours.weekday_text;
+              hours.forEach( function (day){
+                $barInfo.find("ul").append("<li>"+day+"</li>");
+              });
+
+              //style first word in the hours list
+              this.$(".bar-info").find("li").each(function (){
+                 var word = $(this).html();
+                 var index = word.indexOf(' ');
+                 if(index == -1) {
+                    index = word.length;
+                 }
+                 $(this).html('<span class="first-word">' + word.substring(0, 3) + '</span>' + word.substring(index, word.length));
+              });
+
+            };
+          }.bind(this));
+        };
+      }.bind(this));
+
       var liquors = this.liquorTypes(this.catCollection);
       var bars = this.bars();
       var temp = this.template['barTemp']({category: this.category, liquors: liquors, bars: bars, target: target});
