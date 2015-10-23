827b5a8b4c8fb23d1fb1cf58b0f18454f0bc936e
Ariel <arielqhou@gmail.com>
2015-10-23 14:31:09 -0400

    Add rating icons to rating#Form and get rid of radio buttons.

--- app/assets/javascripts/views/ratings/form.js
+++ app/assets/javascripts/views/ratings/form.js
@@ -3,13 +3,52 @@ Cocktailist.Views.RatingForm = Backbone.CompositeView.extend({
 
   events: {
     "submit" : "createRating",
-    "click #delete-rating" : "deleteRating"
+    "click #delete-rating" : "deleteRating",
+    "mouseenter .rating-click-box" : "populateRating",
+    "mouseleave .rating" : "clearRating",
+    "click .rating-click-box" : "setRating"
   },
 
   initialize: function (options){
     //model: rating
     //collection: ratings
     this.cocktail = options.cocktail;
+    if(this.model.get('rating') && this.model.get('rating')>0){
+      this.ratingValue = this.model.get('rating');
+    } else{
+      this.ratingValue = -1;
+    };
+  },
+
+  populateRating: function (e){
+    e.preventDefault();
+    var $cont = this.$el.find(".rating");
+    $cont.find("img").remove();
+    var rating = parseInt($(e.currentTarget).data("rating"));
+    for(var i = 0; i < rating; i++){
+      $cont.append("<img src='https://s3.amazonaws.com/cocktailist-pro/cocktails/imgs/rating-full.png' alt='*'>");
+    };
+  },
+
+  clearRating: function (e){
+    e.preventDefault();
+    var $cont = this.$el.find(".rating");
+    $cont.find("img").remove();
+    this.displayRating();
+  },
+
+  setRating: function (e){
+    e.preventDefault();
+    this.ratingValue = parseInt($(e.currentTarget).data("rating"));
+  },
+
+  displayRating: function(){
+    if(this.ratingValue > 0){
+      var $cont = this.$el.find(".rating");
+      for(var i = 0; i < this.ratingValue; i++){
+        $cont.append("<img src='https://s3.amazonaws.com/cocktailist-pro/cocktails/imgs/rating-full.png' alt='*'>");
+      }
+    }
   },
 
   createRating: function (e){
@@ -17,11 +56,11 @@ Cocktailist.Views.RatingForm = Backbone.CompositeView.extend({
     this.$el.find("button").prop("disabled", true);
     var formData = this.$el.find("form").serializeJSON().rating;
     formData.cocktail_id = this.cocktail.id;
+    formData.rating_num = this.ratingValue;
     this.model.save(formData, {
       success: function(){
         this.cocktail._userRatingId=this.model.id;
         this.collection.add(this.model);
-        // this.$el.find("form").find("input").val("");
         this.$el.find("button").prop("disabled", false);
       }.bind(this),
       error: function (data, res){
@@ -46,6 +85,7 @@ Cocktailist.Views.RatingForm = Backbone.CompositeView.extend({
   render: function (){
     var template = this.template({rating: this.model});
     this.$el.html(template);
+    this.displayRating();
     return this;
   }
 });
