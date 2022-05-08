0a9bddf1084142df2bb355cd8c9e4a26bb975b78
Ariel <arielqhou@gmail.com>
2022-05-08 00:01:36 -0400

    Add aws region to paperclip config.

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,8 @@ module Cocktailist
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
-        :secret_access_key => ENV["s3_secret_access_key"]
+        :secret_access_key => ENV["s3_secret_access_key"],
+        :s3_region => ENV['aws_region']
       }
     }
 
